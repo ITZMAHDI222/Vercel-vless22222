04eedcb5da12be53a12be3450460db64895a19cd
ITZMAHDI222 <itzrealgodemail@gmail.com>
2026-04-14 11:30:13 +0330

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -9,20 +9,20 @@ export default async function handler(req, res) {
 }
 
 function generateNodes() {
-    const vercelDomain = "your-project-name.vercel.app"; // 替换为您的 Vercel 项目域名
-    const uuid1 = "412b4238-204e-4c17-893d-14f13b5dde4f"; // 替换为您的 UUID
+    const vercelDomain = "vercel-vless22222.vercel.app"; // 替换为您的 Vercel 项目域名
+    const uuid1 = "ec27e359-b901-483e-9c57-c406f5e4a4ca"; // 替换为您的 UUID
 
     const nodes = [
         {
             v: "2",
             ps: "Vercel 节点 1",
-            add: "proxy.zhu-di.us.kg", // 使用 Vercel 域名
+            add: "vercel-vless22222.vercel.app", // 使用 Vercel 域名
             port: "443", // Vercel 默认使用 443 端口
             id: uuid1,
             aid: "64",
             net: "ws",
             type: "vmess",
-            host: "proxy.zhu-di.us.kg",
+            host: "vercel-vless22222.vercel.app",
             path: "/api/proxy", // API 路径
             tls: "tls", // 启用 TLS
         },
